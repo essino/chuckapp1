7f03bd593368ca2259bdc3274f6fb6bea5a2a32c
Anni <annikark@metropolia.fi>
2020-09-30 14:48:09 +0300

    Yhteys tietokantaan

---
+++ db.js
@@ -0,0 +1,34 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const dbConnectionUrl = "mongodb+srv://Anni:Auvo100@testailu.08h7w.mongodb.net/Kuvat?retryWrites=true&w=majority";
+
+function initialize(
+    dbName,
+    dbCollectionName,
+    successCallback,
+    failureCallback
+)
+
+
+
+
+{
+    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbCollection);
+        }
+    });
+}
+
+
+module.exports = {
+    initialize
+};
+
---
+++ server.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const server = express();
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "Kuvat";
+const collectionName = "Kuvat";
+
+// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    // << db CRUD routes >>
+
+}, function(err) { // failureCallback
+    throw (err);
+});
+
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
