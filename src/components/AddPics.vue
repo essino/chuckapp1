e79c7662f4453a2025b207e5d17ad4fce7c096b9
Anni <annikark@metropolia.fi>
2020-10-02 17:46:39 +0300

    Söpöyden valinta kuvia lisätessä

--- src/components/AddPics.vue
+++ src/components/AddPics.vue
@@ -5,8 +5,17 @@
         </p>
         <div id="pic-form" @submit.prevent="addPic">
             <form>
-               <label>Id</label>
-                <input v-model="id" type="text" />
+               <label>How do you description cuteness of the animal</label>
+              <input type="radio" id="adorable" value="Adorable" v-model="cuteness">
+              <label for="adorable">Adorable</label>
+              <input type="radio" id="lovely" value="Lovely" v-model="cuteness">
+              <label for="lovely">Lovely</label>
+              <input type="radio" id="supercute" value="Supercute" v-model="cuteness">
+              <label for="supercute">Supercute</label>
+              <input type="radio" id="incredible" value="Incredible" v-model="cuteness">
+              <label for="incredible">Incredible</label>
+              <input type="radio" id="chuckNorris" value="ChuckNorris" v-model="cuteness">
+              <label for="chuckNorris">Chuck Norris</label>
                 <label>Name</label>
                 <input v-model="name" type="text" />
                 <label>Address</label>
@@ -24,11 +33,11 @@
         name: "AddPics",
         data() {
             return {
-                id: '',
+              cuteness: '',
                 name: '',
                 address: '',
                 newPic: {
-                    id: '',
+                  cuteness: '',
                     name: '',
                     address: ''
                 }
@@ -36,14 +45,14 @@
         },
         methods: {
             addPic(){
-                //this.newPic.id = this.id;
+                this.newPic.cuteness = this.cuteness;
                 this.newPic.name = this.name;
                 this.newPic.address = this.address;
                 //this.newTask.done = Boolean(JSON.parse(this.picked));
                 axios.post('http://localhost:4000/items', this.newPic)
                     .then(res => {
                             console.log(res);
-                            this.id = '';
+                            this.cuteness = '';
                             this.name = '';
                             this.address = '';
                         }
