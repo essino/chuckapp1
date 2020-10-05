376199097632700c9a346c6c1c7a37f4300b1913
Anni <annikark@metropolia.fi>
2020-10-05 16:31:30 +0300

    Kuvien suodatus

--- src/components/AddPics.vue
+++ src/components/AddPics.vue
@@ -1,82 +1,80 @@
 <template>
-    <div>
-        <p class="homeText">
-            Add Pics
+  <div>
+    <p class="homeText">
+      Add Pics
+    </p>
+    <div id="pic-form" @submit.prevent="addPic">
+      <form>
+        <p>
+          <label>What is the cuteness level of the animal?</label>
+          <input type="radio" id="adorable" value="Adorable" v-model="cuteness">
+          <label for="adorable">Adorable</label>
+          <input type="radio" id="lovely" value="Lovely" v-model="cuteness">
+          <label for="lovely">Lovely</label>
+          <input type="radio" id="supercute" value="Supercute" v-model="cuteness">
+          <label for="supercute">Supercute</label>
+          <input type="radio" id="incredible" value="Incredible" v-model="cuteness">
+          <label for="incredible">Incredible</label>
+          <input type="radio" id="chuckNorris" value="ChuckNorris" v-model="cuteness">
+          <label for="chuckNorris">Chuck Norris</label>
         </p>
-        <div id="pic-form" @submit.prevent="addPic">
-            <form>
-                <p>
-                    <label>What is the cuteness level of the animal?</label>
-                    <input type="radio" id="adorable" value="Adorable" v-model="cuteness">
-                    <label for="adorable">Adorable</label>
-                    <input type="radio" id="lovely" value="Lovely" v-model="cuteness">
-                    <label for="lovely">Lovely</label>
-                    <input type="radio" id="supercute" value="Supercute" v-model="cuteness">
-                    <label for="supercute">Supercute</label>
-                    <input type="radio" id="incredible" value="Incredible" v-model="cuteness">
-                    <label for="incredible">Incredible</label>
-                    <input type="radio" id="chuckNorris" value="ChuckNorris" v-model="cuteness">
-                    <label for="chuckNorris">Chuck Norris</label>
-                </p>
-                <p>
-                    <label>The animal's name</label>
-                    <input v-model="name" type="text" />
-                </p>
-                <p>
-                    <label>The link</label>
-                    <input v-model="address" type="text" />
-                </p>
-              <p><img :src="address" alt="This isn't your pic!"/></p>
-                <button>Add Pic</button>
-            </form>
-        </div>
+        <p>
+          <label>The animal's name</label>
+          <input v-model="name" type="text" />
+        </p>
+        <p>
+          <label>The link</label>
+          <input v-model="address" type="text" />
+        </p>
+        <p><img :src="address" alt="This isn't cute pic!"/></p>
+        <button>Add Pic</button>
+      </form>
     </div>
+  </div>
 </template>
 
 
 
 <script>
-    import axios from 'axios';
-
-
-    export default {
-        name: "AddPics",
-        data() {
-            return {
-              cuteness: '',
-                name: '',
-                address: '',
-                newPic: {
-                  cuteness: '',
-                    name: '',
-                    address: ''
-                }
-            }
-        },
-        methods: {
-            addPic(){
-                this.newPic.cuteness = this.cuteness;
-                this.newPic.name = this.name;
-                this.newPic.address = this.address;
-                //this.newTask.done = Boolean(JSON.parse(this.picked));
-                axios.post('http://localhost:4000/items', this.newPic)
-                    .then(res => {
-                            console.log(res);
-                            this.cuteness = '';
-                            this.name = '';
-                            this.address = '';
-                        }
-                    )
-            }
-        }
+import axios from 'axios';
+export default {
+  name: "AddPics",
+  data() {
+    return {
+      cuteness: '',
+      name: '',
+      address: '',
+      newPic: {
+        cuteness: '',
+        name: '',
+        address: ''
+      }
     }
+  },
+  methods: {
+    addPic(){
+      this.newPic.cuteness = this.cuteness;
+      this.newPic.name = this.name;
+      this.newPic.address = this.address;
+      //this.newTask.done = Boolean(JSON.parse(this.picked));
+      axios.post('http://localhost:4000/items', this.newPic)
+          .then(res => {
+                console.log(res);
+                this.cuteness = '';
+                this.name = '';
+                this.address = '';
+              }
+          )
+    }
+  }
+}
 </script>
 
 <style scoped>
 label, input {
-    padding-right: 0.5em;
+  padding-right: 0.5em;
 }
-    img{
-        width: 10%;
-    }
+img{
+  width: 10%;
+}
 </style>
--- src/components/ShowPics.vue
+++ src/components/ShowPics.vue
@@ -7,6 +7,24 @@
             <thead>
             <tr>
                 <th>Pics</th>
+             <div id="pic-form" @submit.prevent="showPics">
+              <form>
+              <p>
+                <label>What is the cuteness level of the animal?</label>
+                <input type="radio" id="adorable" value="Adorable" v-model="selectCuteness">
+                <label for="adorable">Adorable</label>
+                <input type="radio" id="lovely" value="Lovely" v-model="selectCuteness">
+                <label for="lovely">Lovely</label>
+                <input type="radio" id="supercute" value="Supercute" v-model="selectCuteness">
+                <label for="supercute">Supercute</label>
+                <input type="radio" id="incredible" value="Incredible" v-model="selectCuteness">
+                <label for="incredible">Incredible</label>
+                <input type="radio" id="chuckNorris" value="ChuckNorris" v-model="selectCuteness">
+                <label for="chuckNorris">Chuck Norris</label>
+              </p>
+              <button>Add Pic</button>
+              </form>
+             </div>
             </tr>
             </thead>
             <tbody>
@@ -26,29 +44,58 @@
     import axios from 'axios';
 
     export default {
-        name: "ShowPics",
-        data() {
-            return {
-                pics: [],
-                pic: {
-                    id: '',
-                    name: '',
-                    address: ''
-                }
-            }
-        },
-        async created() {
-            try {
-
-                const promise = await axios.get('http://localhost:4000/items');
-                //const promise = await axios.get('https://my-json-server.typicode.com/essino/fakedb/events1');
-                this.pics = promise.data;
-
-            } catch (e) {
-                console.error(e)
-            }
+      name: "ShowPics",
+      data() {
+        return {
+          selectCuteness: "",
+          pics: [],
+          pic: {
+            id: '',
+            name: '',
+            address: ''
+          }
         }
+      },
+
+      async created() {
+
+
+        {
+          console.log(this.selectCuteness);
+          try {
+            console.log(this.selectCuteness);
+            {
+              const promise = await axios.get('http://localhost:4000/items/');
+              //const promise = await axios.get('https://my-json-server.typicode.com/essino/fakedb/events1');
+              this.pics = promise.data;
+
+            }/*else {
+                const promise =  axios.get('http://localhost:4000/cutenesses/' + this.selectCuteness);
+                //const promise = await axios.get('https://my-json-server.typicode.com/essino/fakedb/events1');
+                console.log("Menen tänne vaikka ei pitäisi")
+                this.pics = promise.data;
+              }*/
+
+
+          } catch (e) {
+            console.error(e)
+          }
+        }
+      },
+      methods: {
+        async showPics() {
+           {
+
+              const promise = await axios.get('http://localhost:4000/cutenesses/' + this.selectCuteness);
+              //const promise = await axios.get('https://my-json-server.typicode.com/essino/fakedb/events1');
+
+              this.pics = promise.data;
+          }
+        }
+        //}
+      }
     }
+
 </script>
 
 <style scoped>
